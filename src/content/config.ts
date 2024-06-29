7d4f9bba2a353e12bc4e5d2745865938f4f3b4e0
Ethan Niser <ethan@niser.com>
2024-06-29 14:55:42 -0700

    init

---
+++ src/content/config.ts
@@ -0,0 +1,14 @@
+import { defineCollection, z } from "astro:content";
+
+const blog = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    tags: z.array(z.string()).optional(),
+  }),
+});
+
+export const collections = { blog };
