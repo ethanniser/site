00a2ed98c53e7fcc61a2763a50025cebfcbac085
Ethan Niser <ethan@niser.com>
2024-06-30 14:32:27 -0700

    loosen ts config a bit

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,6 +4,7 @@ const blog = defineCollection({
   type: "content",
   schema: z.object({
     title: z.string(),
+    description: z.string().optional(),
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
     tags: z.array(z.string()).optional(),
