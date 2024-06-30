7175dba185cbd9af57d5a26a5b84f85f0e96a76d
Ethan Niser <ethan@niser.com>
2024-06-30 13:21:28 -0700

    blog card

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,7 +4,6 @@ const blog = defineCollection({
   type: "content",
   schema: z.object({
     title: z.string(),
-    description: z.string(),
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
     tags: z.array(z.string()).optional(),
