768e24e0a29f064f25034591dfa20643ce1e73ea
Ethan Niser <ethan@niser.com>
2024-06-30 23:48:16 -0700

    improve responsiveness

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,14 @@ export default {
         sans: ['"Berkeley Mono Trial"', "sans-serif"],
       },
     },
+    screens: {
+      xs: "480px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
