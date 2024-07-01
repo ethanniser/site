6dde500b42f4123cf64d42e86961b7bc7c7b6594
Ethan Niser <ethan@niser.com>
2024-07-01 01:29:05 -0700

    try out ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ethanniser.dev",
   integrations: [mdx(), sitemap(), tailwind()],
+  output: "server",
+  adapter: vercel(),
 });
