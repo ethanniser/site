93b7bd4baac823b96bdd15b4726b71dca93339d8
Ethan Niser <ethan@niser.com>
2024-07-01 01:52:36 -0700

    oops

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
 
 import cloudflare from "@astrojs/cloudflare";
 
@@ -11,5 +10,5 @@ export default defineConfig({
   site: "https://ethanniser.dev",
   integrations: [mdx(), sitemap(), tailwind()],
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
 });
