971b457f161bb694bb877e4d4622912193f59657
Ethan Niser <ethan@niser.com>
2024-07-01 21:27:21 -0700

    prettier

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,5 +1,11 @@
 // .prettierrc.mjs
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+  plugins: [
+    "prettier-plugin-astro",
+    "prettier-plugin-tailwindcss",
+    "prettier-plugin-classnames",
+    "prettier-plugin-merge",
+  ],
+  endingPosition: "relative",
 };
