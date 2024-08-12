a26e3ca5b8eacd5d5ff4834e88109a16f2a4e01f
Ethan Niser <ethan@niser.com>
2024-08-12 18:46:16 -0500

    update email

--- src/consts.ts
+++ src/consts.ts
@@ -1,13 +1,10 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = "Ethan Niser";
 export const SITE_DESCRIPTION = "My personal website";
 export const TWITTER_LINK = "https://twitter.com/ethanniser";
 export const GITHUB_LINK = "https://github.com/ethanniser";
 export const YOUTUBE_LINK = "https://www.youtube.com/@ethanniser";
 export const LINKEDIN_LINK = "https://www.linkedin.com/in/ethanniser/";
-export const EMAIL_LINK = "mailto:ethan.niser@gmail.com";
+export const EMAIL_LINK = "mailto:ethan@niser.com";
 export const JS_KEYWORDS = [
   "break",
   "case",
