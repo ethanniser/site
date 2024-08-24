b723845b1dd04f5a0da8e82a1aad622ee6050ee5
Ethan Niser <ethan@niser.com>
2024-08-24 12:16:12 -0400

    initial video components

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly GOOGLE_API_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
