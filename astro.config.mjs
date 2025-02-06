fdfa5c0afa4e030b5d850f550828809817e62b49
Ethan Niser <ethan@niser.com>
2025-02-05 23:26:13 -0500

    upgrade to tailwind 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
 import cloudflare from "@astrojs/cloudflare";
 import remarkToc from "remark-toc";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
-
+import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 
 // https://astro.build/config
@@ -32,11 +31,13 @@ export default defineConfig({
       extendMarkdownConfig: true,
     }),
     sitemap(),
-    tailwind(),
     react(),
   ],
   output: "server",
   adapter: cloudflare({
     imageService: "cloudflare",
   }),
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
