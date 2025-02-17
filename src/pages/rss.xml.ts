aecfc75480ff9f0ae3102d9e2d53ccaa5ce90429
Ethan Niser <ethan@niser.com>
2025-02-17 02:32:36 -0500

    fix rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,8 +1,12 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
+import type { APIRoute } from "astro";
 
-export async function GET(context) {
+export const GET: APIRoute = async (context) => {
+  if (!context.site) {
+    throw new Error("No site - check astro config");
+  }
   const posts = await getCollection("blog");
   return rss({
     title: SITE_TITLE,
@@ -10,7 +14,7 @@ export async function GET(context) {
     site: context.site,
     items: posts.map((post) => ({
       ...post.data,
-      link: `/blog/${post.slug}/`,
+      link: `/blog/${post.id}/`,
     })),
   });
-}
+};
